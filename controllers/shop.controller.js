5b7c92547d647980086806f23d4adb1e6e7991bd
Glitch (28-error-handling) <none>
2020-05-26 14:24:07 +0000

    🏢🌑 Checkpoint ./models/shop.model.js:15927824/139 ./controllers/shop.controller.js:15927824/527 ./views/shop/index.pug:15927824/40

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var cloudinary = require("cloudinary").v2;
 
-var Book = require("../models/book.model.js");
+var Shop = require("../models/shop.model.js");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -10,9 +10,20 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 });
 
-module.exports.index = function(req, res) {
+module.exports.index = async function(req, res) {
   var userId = req.params.id;
-  res.render("shop/index");
+  var err;
+  
+  var shop = await Shop.findOne({ userId: userId });
+ 
+  if (!shop.books) {
+    err = 'No book in your shop!'
+  }
+  
+  res.render("shop/index", {
+    books: shop.books,
+    err: err
+  });
 };
 
 module.exports.create = function(req, res) {
@@ -20,23 +31,23 @@ module.exports.create = function(req, res) {
 };
 
 module.exports.postCreate = function(req, res) {
-  var title = req.body.title;
-  var des = req.body.description;
-  var image = "https://29-my-shop.glitch.me/" + req.file.path.slice(7);
+//   var title = req.body.title;
+//   var des = req.body.description;
+//   var image = "https://29-my-shop.glitch.me/" + req.file.path.slice(7);
 
-  cloudinary.uploader.upload(image, function(error, result) {
-    console.log(result, error);
-    var book = new Book({
-      title: title,
-      description: des,
-      coverUrl: result.url
-    });
+//   cloudinary.uploader.upload(image, function(error, result) {
+//     console.log(result, error);
+//     var book = new Book({
+//       title: title,
+//       description: des,
+//       coverUrl: result.url
+//     });
     
-    book.save(function (err, book) {
-      if (err) return console.error(err);
-      console.log(book.name + " saved to bookstore collection.");
-    });
-  });
+//     book.save(function (err, book) {
+//       if (err) return console.error(err);
+//       console.log(book.name + " saved to bookstore collection.");
+//     });
+//   });
 
   res.redirect("/shops/books");
 };
--- models/shop.model.js
+++ models/shop.model.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
 
 var shopSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  coverUrl: String
+  userId: String,
+  books: {
+    id: String,
+    title: String,
+    description: String,
+    coverUrl: String
+  }
 }, { versionKey: false });
 
-var Shop = mongoose.model('Shop', shopSchema, 'sh');
+var Shop = mongoose.model('Shop', shopSchema, 'shops');
 
 module.exports = Shop;
