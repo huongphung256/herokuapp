3aed403d06fabc2ef5595e321b3ba4019b22c399
Glitch (28-error-handling) <none>
2020-05-26 15:58:33 +0000

    ⛺️😂 Checkpoint ./middleware/auth.middleware.js:15927824/314

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,12 +1,14 @@
 var db = require("../db");
 var User = require("../models/user.model.js");
 
-module.exports.authLogin = function(req, res, next) {
+module.exports.authLogin = async function(req, res, next) {
     var cookie = req.signedCookies.userId;
   
     // var user = db.get("users").find({ id: cookie }).value();
-    var user = User.findById({ _id: cookie}).then(result => res.locals.user = result);
-    console.log(res.locals.user);
+    var user = await User.findById({ _id: cookie});
+  
+    // var user = User.findOne({ _id: cookie});
+    console.log(res.locals.user, user);
     
     if (!cookie) {
       res.redirect('/auth/login');
